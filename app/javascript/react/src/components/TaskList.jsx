be28cc348a37e3c00f54a120c1dfd12cf8373d6a
bhagathreddy123 <katta_reddy04@yahoo.com>
2024-02-11 21:27:42 -0800

    fetching API response in react component

--- app/javascript/react/src/components/TaskList.jsx
+++ app/javascript/react/src/components/TaskList.jsx
@@ -1,41 +1,27 @@
 import * as React from 'react'
+import { useState, useEffect } from 'react'
 import * as ReactDom from 'react-dom'
 import TaskDetail from './TaskDetail'
 
 const TaskList = () => {
-	const tasksList = [
-	{ 
-		id: 1,
-		title: "React",
-		description: "open-source front-end JavaScript library"
-	},
-	{ 
-		id: 2,
-		title: "major features of React",
-		description: "JSX syntax"
-	},
-	{ 
-		id: 3,
-		title: "DevTools",
-		description: "open Chrome Extensions and check Allow access to file URLs"
-	},
-	{ 
-		id: 4,
-		title: "Chrome for local files",
-		description: "open Chrome Extensions and check Allow access to file URLs"
-	},
-	{ 
-		id: 5,
-		title: "advantages of React over Vue.js",
-		description: "Gives more flexibility in large apps developing"
-	}
+	const [taskList, setTaskList] = useState([])
+	const tasksUrl = 'http://localhost:3000/api/v1/tasks'
 
-	]
+	const fetchTaskList = () => {
+	  fetch(tasksUrl)
+	    .then((response) => response.json())
+		.then((data) => {
+			setTaskList(data)
+		})
+	}
+	useEffect(() => {
+		fetchTaskList()
+	}, [])
 
 	return (
 		<div className="row">
 			<div className="col-lg-10 mx-auto">
-			 {tasksList.map((task) =>  
+			 {taskList.map((task) =>  
 	             < TaskDetail task={task} key={task.id} />
 		     )}
 			</div>		
