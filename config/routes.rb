810746e6e50f56e2258b838c9c0e9629c2f27894
bhagathreddy123 <katta_reddy04@yahoo.com>
2024-02-11 20:39:23 -0800

    created rails api end point to fetch task data

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
+  namespace :api do 
+    namespace :v1 do
+      resources :tasks, only: :index
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Creating Tasks"
+tasks = Task.create([
+
+    { 
+		title: "React",
+		description: "open-source front-end JavaScript library"
+	},
+
+	{ 
+		title: "major features of React",
+		description: "JSX syntax"
+	},
+
+	{ 
+		title: "DevTools",
+		description: "open Chrome Extensions and check Allow access to file URLs"
+	},
+
+	{ 
+		title: "Chrome for local files",
+		description: "open Chrome Extensions and check Allow access to file URLs"
+	},
+
+	{ 
+		title: "advantages of React over Vue.js",
+		description: "Gives more flexibility in large apps developing"
+	}
+])
+puts "Tasks Created"
