2301b2999db8a0952b790ae5a39d9090646dfe30
bhagathreddy123 <katta_reddy04@yahoo.com>
2024-02-14 12:55:16 -0800

    testing seed data for users and tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,48 @@
 puts "Creating Tasks"
+users =  User.create!(
+  [
+    {first_name: "Jan", last_name: 'Doe', email: 'jan@example.com', password: "jan12345", password_confirmation: "jan12345"},
+    {first_name: "Jane", last_name: 'Doe', email: 'jane@example.com', password: "jane12345", password_confirmation: "jane12345"}
+
+  ]
+
+)
+
 tasks = Task.create([
 
     { 
 		title: "React",
-		description: "open-source front-end JavaScript library"
+		description: "open-source front-end JavaScript library",
+		user_id: 1,
+		status: 'To Do'
 	},
 
 	{ 
 		title: "major features of React",
-		description: "JSX syntax"
+		description: "JSX syntax",
+		user_id: 1,
+		status: 'In Progress'
 	},
 
 	{ 
 		title: "DevTools",
-		description: "open Chrome Extensions and check Allow access to file URLs"
+		description: "open Chrome Extensions and check Allow access to file URLs",
+		user_id: 2,
+		status: 'In Progress'
 	},
 
 	{ 
 		title: "Chrome for local files",
-		description: "open Chrome Extensions and check Allow access to file URLs"
+		description: "open Chrome Extensions and check Allow access to file URLs",
+		user_id: 2,
+		status: 'To Do'
 	},
 
 	{ 
 		title: "advantages of React over Vue.js",
-		description: "Gives more flexibility in large apps developing"
+		description: "Gives more flexibility in large apps developing",
+		user_id: 2,
+		status: 'In Progress'
 	}
 ])
 puts "Tasks Created"
